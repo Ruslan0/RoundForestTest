63927c01cfc3381d49f7bac31b4a40879af1ad5b
Ruslan <Ruslan@Ruslan-PC>
2017-06-06 23:06:23 +0300

    checkstyle

--- src/by/roundforest/service/Reviews.java
+++ src/by/roundforest/service/Reviews.java
@@ -6,7 +6,11 @@ import by.roundforest.dto.ViewBean;
 import by.roundforest.dto.WordBean;
 
 public interface Reviews {
-	 List<ViewBean> getActiveUsers(int count);
-	 List<ViewBean> getCommentedFoodItems(int count);
-	 List <WordBean> getMostUsedWords(int count);
+  
+  List<ViewBean> getActiveUsers(int count);
+  
+  List<ViewBean> getCommentedFoodItems(int count);
+  
+  List<WordBean> getMostUsedWords(int count);
+  
 }
