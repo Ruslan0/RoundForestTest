e98a6e214ae0841260ef872082622f47a56e2539
Ruslan <Ruslan@Ruslan-PC>
2017-06-06 23:00:46 +0300

    Checkstyle

--- src/by/roundforest/impl/ReviewsImpl.java
+++ src/by/roundforest/impl/ReviewsImpl.java
@@ -23,10 +23,8 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 
 public class ReviewsImpl implements Reviews {
 
-  private final String sqlActiveUsers = 
-      "select UserId, profileName, count(*) count from Reviews group by UserId order by 3 desc limit ?";
-  private final String sqlCommentedFoodItems =
-      "select ProductId, count(*) count from Reviews group by ProductId order by 2 desc limit ?";
+  private final String sqlActiveUsers =  "select UserId, profileName, count(*) count from Reviews group by UserId order by 3 desc limit ?";
+  private final String sqlCommentedFoodItems = "select ProductId, count(*) count from Reviews group by ProductId order by 2 desc limit ?";
   private final String sqlAll = "select Text from Reviews";
   private static JdbcTemplate jdbcTemplate = (JdbcTemplate)
       new ClassPathXmlApplicationContext("Beans.xml").getBean("jdbcTemplate");
@@ -52,40 +50,41 @@ public class ReviewsImpl implements Reviews {
        new BeanPropertyRowMapper<ViewBean>(ViewBean.class));
   }
 
-	@Override
-	public List<ViewBean> getCommentedFoodItems(int count) {
-		return jdbcTemplate.query(sqlCommentedFoodItems,
-				new Object[] {count},
-				new BeanPropertyRowMapper<ViewBean>(ViewBean.class));
-	}
+  @Override
+  public List<ViewBean> getCommentedFoodItems(int count) {
+    return jdbcTemplate.query(sqlCommentedFoodItems,
+      new Object[] {count},
+      new BeanPropertyRowMapper<ViewBean>(ViewBean.class));
+  }
 
-	@Override
-	public List<WordBean> getMostUsedWords(int count) {
+  @Override
+  public List<WordBean> getMostUsedWords(int count) {
 
-		Comparator<WordBean> comp = (WordBean o1, WordBean o2) -> (o1.compareToWord(o2));
-		Comparator<WordBean> compWeight = (WordBean o1, WordBean o2) -> (o1.compareToWeight(o2));
-		  
-		TreeSet<WordBean> wordsSet = jdbcTemplate.query(sqlAll, new ResultSetExtractor<TreeSet<WordBean>>(){
-		  @Override
-		  public TreeSet<WordBean> extractData(ResultSet rs) throws SQLException, DataAccessException {
-			  TreeSet<WordBean> wordBeanSet=new TreeSet<WordBean>(comp);
-			  while(rs.next()){
-				  
-				  String rowDeatil=rs.getString(1);
-		          List<String> words =  Arrays.asList(rowDeatil.replaceAll("[^a-zA-Z]", " ").toLowerCase().split(" "));
-		          words.forEach(item->wordBeanSet.add(new WordBean(item)));
-			  }  				
-			  return wordBeanSet;
-		  }
-		});
-		List <WordBean> wordsSort=new ArrayList(Arrays.asList(wordsSet.stream().toArray()));
-		Collections.sort(wordsSort, compWeight);
-		
-		return wordsSort.subList(0, count);
-	}
+    Comparator<WordBean> comp = (WordBean o1, WordBean o2) -> (o1.compareToWord(o2));
+    Comparator<WordBean> compWeight = (WordBean o1, WordBean o2) -> (o1.compareToWeight(o2));
 
-	public static void main(String[] args) {
-		new ReviewsImpl().getReviews();
-	}
+    TreeSet<WordBean> wordsSet = 
+        jdbcTemplate.query(sqlAll, new ResultSetExtractor<TreeSet<WordBean>>(){
+          @Override
+          public TreeSet<WordBean> extractData(ResultSet rs) throws SQLException, 
+              DataAccessException {
+            TreeSet<WordBean> wordBeanSet = new TreeSet<WordBean>(comp);
+            while (rs.next()) {
+              String rowDeatil = rs.getString(1);
+                List<String> words =  Arrays.asList(rowDeatil.replaceAll("[^a-zA-Z]", " ").toLowerCase().split(" "));
+                words.forEach(item -> wordBeanSet.add(new WordBean(item)));
+            } 
+            return wordBeanSet;
+          }
+        });
+    List<WordBean> wordsSort = new ArrayList(Arrays.asList(wordsSet.stream().toArray()));
+    Collections.sort(wordsSort, compWeight);
+
+    return wordsSort.subList(0, count);
+  }
+
+  public static void main(String[] args) {
+    new ReviewsImpl().getReviews();
+  }
 
 }
